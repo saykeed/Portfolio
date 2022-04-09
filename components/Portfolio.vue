59c2707aace3552dee1b4786a1a73af87b543795
saykeed <saykeed@gmail.com>
2022-04-09 23:03:43 +0100

    deleted some comments that might stop it from deploying on netlify

--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -47,24 +47,3 @@ export default {
     }
 </style>
 
-
-
-/*  
-
-<div class="portfolio">
-        <h1>E-commerce</h1>
-        <p>
-            I was tired of switching between browser, code editor, To-do app and a Timer app. So I created Nest Tab.
-        </p>
-        <img src="@/assets/img/portimg.JPG" alt="">
-        <div class="portBtn">
-            <a href="/">Live Demo</a>
-            <a href="/">Code on Github</a>
-        </div>
-        <p>
-            Nest Tab is a simple Google Chrome extension that replaces your New Tab page with a clean looking page that contains tools to help you stay productive. It currently features a Todo list and a Pomodoro clock that works together, a Shelf to dump URLs that you want to take a look at later, and a Bookmark quick access tool. Nest Tab is created using React. See the repo for more details.
-        </p>
-    </div>
-
-
-*/
